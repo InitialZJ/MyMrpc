0850243341d42e0535481f08068e4f1265d98530
InitialZJ <blueskyzzz0418@gmail.com>
2023-12-26 14:48:53 +0800

    添加框架应用类

--- src/include/MrpcApplication.h
+++ src/include/MrpcApplication.h
@@ -1,6 +1,21 @@
 #ifndef MRPC_APPLICATION_H_
 #define MRPC_APPLICATION_H_
 
-#include
+#include "MrpcConfig.h"
+
+// 框架类，用单例模式进行设计
+class MrpcApplication {
+ public:
+  static void Init(int argc, char** argv);
+  static MrpcApplication& GetInstance();
+  static MrpcConfig& getConfig();
+
+ private:
+  static MrpcConfig m_config;
+  MrpcApplication(){};
+  MrpcApplication(const MrpcApplication& other) = delete;
+  MrpcApplication(MrpcApplication&& other) = delete;
+  MrpcApplication& operator=(const MrpcApplication& other) = delete;
+};
 
 #endif  // !MRPC_APPLICATION_H_
