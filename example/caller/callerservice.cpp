c40ee62884e2f905fd7a2d847ff1d60092dac95c
InitialZJ <blueskyzzz0418@gmail.com>
2023-12-28 20:03:57 +0800

    完善caller代码

--- example/caller/callerservice.cpp
+++ example/caller/callerservice.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
 #include "MrpcApplication.h"
 #include "MrpcChannel.h"
+#include "MrpcController.h"
+#include "user.pb.h"
 
+int main(int argc, char** argv) {
+	MrpcApplication::Init(argc, argv);
+}
